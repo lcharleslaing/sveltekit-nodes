04958c4b8c967bf319a7ff4e606af0f4004d0649
leecharleslaing <71520336+lcharleslaing@users.noreply.github.com>
2022-03-13 10:39:32 -0400

    replaced title with name as the last link folder

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,11 +4,29 @@
   let body = [];
   for (let path in modules) {
     let pathSanitized = path.replace(".svelte", "").replace("./", "/");
+
+    let title1 = pathSanitized.substring(pathSanitized.lastIndexOf("/") + 1);
+
+    let link1 = pathSanitized.includes("index")
+      ? pathSanitized.replace("/index", "")
+      : pathSanitized;
+    let link2 = link1.replace("", "/").replace("//", "/");
+    let title2 = link2 === "/" ? title1.replace("index", "home") : title1;
+
+    let title3 = link2.split("/");
+    let indexName = title3[title3.length - 1];
+
+    let title4 =
+      title2 === "index" ? title2.replace("index", indexName) : title2;
+
+    // console.log(indexName);
+
+    let title = title4;
+    let link = link2;
+
     body.push({
-      title: pathSanitized.substring(pathSanitized.lastIndexOf("/") + 1),
-      link: pathSanitized.includes("index")
-        ? pathSanitized.replace("/index", "")
-        : pathSanitized,
+      title,
+      link,
     });
   }
   console.log(body);
